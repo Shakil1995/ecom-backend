17d47b0534c774e2818c088dad90750ac26aa12e
Nurul Amin Shakil <41535333+Shakil1995@users.noreply.github.com>
2022-05-17 11:07:49 +0600

    category added

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Http\Requests\Category\UpdateCategoryRequest;
 
 class CategoryController extends Controller
 {
-    
+    private $_getColumns = (['id', 'name', 'is_active']);
+
     public function index()
     {
-        
+        $categories = Category::get($this->_getColumns);
+
+        return response()->json([
+            'status' => true,
+            'category' => $categories
+        ], 200);
     }
 
    
@@ -20,15 +29,31 @@ class CategoryController extends Controller
         //
     }
 
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+    //    dd($request);
+        $category = new Category; 
+
+        //insert data
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+
+        //save to database
+        $category->save();
+
+        return response()->json([
+            'message' => "Category Created Successfully!!",
+            'category' => $category
+        ], 200);
     }
 
-  
+    private $_showColumns = (['id', 'name', 'is_active']);
     public function show(Category $category)
     {
-        //
+        return response()->json([
+            'message' => "Category Showed Successfully!!",
+            'category' => $category
+        ], 200);
     }
 
   
@@ -38,14 +63,28 @@ class CategoryController extends Controller
     }
 
     
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+
+        //save to database
+        $category->update();
+
+        return response()->json([
+            'message' => "Category updated Successfully!!",
+            'category' => $category
+        ], 200);
     }
 
     
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return response()->json([
+            'message' => "Category Deleted Successfully!!",
+        ], 200);
+    
     }
 }
