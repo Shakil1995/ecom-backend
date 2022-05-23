79ac9e646d207a026f8ad94aa64f290ca4625ddd
Nurul Amin Shakil <41535333+Shakil1995@users.noreply.github.com>
2022-05-23 20:45:11 +0600

    addd price type crud

--- app/Http/Controllers/Api/PriceTypeController.php
+++ app/Http/Controllers/Api/PriceTypeController.php
@@ -2,30 +2,28 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\PriceType;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PriceType\StorePriceTypeRequest;
+use App\Http\Requests\PriceType\UpdatePriceTypeRequest;
 
 class PriceTypeController extends Controller
 {
-   
     public function index()
     {
         $priceTypes = PriceType::all();
 
         return response()->json([
-            'status' => true,
             'priceTypes' => $priceTypes
-        ], 200); 
+        ], 200);
     }
 
-   
     public function create()
     {
-        //
+        
     }
 
-  
     public function store(StorePriceTypeRequest $request)
     {
         $priceType = new PriceType; 
@@ -36,30 +34,22 @@ class PriceTypeController extends Controller
         //save to database
         $priceType->save();
 
-        return response()->json([
-            'message' => "Price Created Successfully!!",
-            'priceType' => $priceType
-        ], 200);
+        return redirect(config('app.frontend_url').'/price-types/index')->with('status','Price Type has been Created Successfully !');
     }
 
-   
     public function show(PriceType $priceType)
     {
-      
         return response()->json([
             'message' => "Price Showed Successfully!!",
             'priceType' => $priceType
         ], 200);
-       
     }
 
-    
     public function edit(PriceType $priceType)
     {
         //
     }
 
-    
     public function update(UpdatePriceTypeRequest $request, PriceType $priceType)
     {
         //insert data
@@ -68,19 +58,22 @@ class PriceTypeController extends Controller
         //save to database
         $priceType->update();
 
-        return response()->json([
-            'message' => "Price updated Successfully!!",
-            'price' => $priceType
-        ], 200);
+        return redirect(config('app.frontend_url').'/price-types/index')->with('status','Price Type has been Updated Successfully !');
     }
 
-   
     public function destroy(PriceType $priceType)
     {
         $priceType->delete();
 
-        return response()->json([
-            'message' => "Price Deleted Successfully!!",
-        ], 200);
+        return redirect(config('app.frontend_url').'/price-types/index')->with('status','Price Type has been Deleted Successfully !');
+    }
+
+    public function toggleStatus(PriceType $priceType)
+    {
+        $priceType->is_active = !$priceType->is_active;
+
+        $priceType->update();
+
+        return redirect(config('app.frontend_url').'/price-types/index')->with('status','PriceType Status has been Toggled Successfully !');
     }
 }
